d72116d009930ff8cb7483429a80417dd8318da0
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-09 21:35:41 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,15 @@
 class Course
 
-  attr_accessor :title, :schedule, :description 
-  
-end 
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+    def initialize
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end 
+
+end
